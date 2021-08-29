63b681ca921380041b4cca5186084222a3a6beb5
Mary Saray <msaray.ms@outlook.com>
2021-08-29 15:46:28 -0700

    Added register for user context into database

--- eCommerceWebsiteExample/Controllers/UserController.cs
+++ eCommerceWebsiteExample/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceWebsiteExample.Data;
+using eCommerceWebsiteExample.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,41 @@ namespace eCommerceWebsiteExample.Controllers
 {
     public class UserController : Controller
     {
+        // field
+        private readonly ProductContext _context;
+        // constructor
+        public UserController(ProductContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                // map data to user account instance
+                UserAccount acc = new UserAccount()
+                {
+                    DateOfBirth = reg.DateOfBirth,
+                    Email = reg.Email,
+                    Password = reg.Password,
+                    Username = reg.Username
+                };
+                // add to database
+                _context.UserAccounts.Add(acc);
+                // query to database
+                await _context.SaveChangesAsync();
+                // after it is saved redirect to home page
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(reg);
         }
     }
 }
