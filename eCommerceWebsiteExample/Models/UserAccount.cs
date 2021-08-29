9b81c8ce2b1767c4ac830985bff684aa8ce61085
Mary Saray <82470838+marysaray@users.noreply.github.com>
2021-08-29 15:50:55 -0700

    Issue#29 create registration (#32)
    * Created a UserAccount model
    
    * Added UserAccount to Database using code first migration
    
    * Created RegisterViewModel class
    
    * Added User Controller
    
    * Created view for Register
    
    * Added validation and display features with data annotations
    
    * Added navigation link to Register view URL
    
    * Added Username to view Register model
    
    * Added Username to RegisterViewModel class
    
    * Added register for user context into database

---
+++ eCommerceWebsiteExample/Models/UserAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceWebsiteExample.Models
+{
+    public class UserAccount
+    {
+        [Key] // primary key: using attached
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime? DateOfBirth { get; set; } // ?: optional
+    }
+
+    public class RegisterViewModel
+    { 
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Email { get; set; }
+
+        [Compare(nameof(Email))]
+        [Required]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)] // password box
+        [StringLength(120, MinimumLength = 6, ErrorMessage = "Password must be between {2} and {1}")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)] // password box
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.Date)] // time is ignored
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
