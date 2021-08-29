c538823e26a085f679ebfac42ae12f9c069381a2
Mary Saray <msaray.ms@outlook.com>
2021-08-29 15:44:41 -0700

    Added Username to RegisterViewModel class

--- eCommerceWebsiteExample/Models/UserAccount.cs
+++ eCommerceWebsiteExample/Models/UserAccount.cs
@@ -23,6 +23,10 @@ namespace eCommerceWebsiteExample.Models
     public class RegisterViewModel
     { 
         [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
         [StringLength(200)]
         public string Email { get; set; }
 
