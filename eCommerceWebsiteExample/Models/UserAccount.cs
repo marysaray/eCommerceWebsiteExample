ac9baefcb05382d1e75c0e33ee2f5d0f3cfae359
Mary Saray <msaray.ms@outlook.com>
2021-08-29 14:14:42 -0700

    Created a UserAccount model

---
+++ eCommerceWebsiteExample/Models/UserAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceWebsiteExample.Models
+{
+    public class UserAccount
+    {
+        [Key] // primary key: using attached
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime? DateOfBirth { get; set; } // ?: optional
+    }
+}
