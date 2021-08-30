1da89423f53d45b5782496b5e689347d9c465e86
Mary Saray <msaray.ms@outlook.com>
2021-08-30 11:53:37 -0700

    Ensure username and email are unique during registration

--- eCommerceWebsiteExample/Controllers/UserController.cs
+++ eCommerceWebsiteExample/Controllers/UserController.cs
@@ -30,6 +30,30 @@ namespace eCommerceWebsiteExample.Controllers
         {
             if (ModelState.IsValid)
             {
+                // Check if username/email is in use!
+                bool isEmailTaken = await (from account in _context.UserAccounts
+                                     where account.Email == reg.Email
+                                     select account).AnyAsync();
+
+                // if so, add custom error and send back to view
+                if (isEmailTaken)
+                {
+                    ModelState.AddModelError(nameof(RegisterViewModel.Email), "Email is already subscribed.");
+                }
+
+                bool isUsernameTaken = await (from account in _context.UserAccounts
+                                              where account.Username == reg.Username
+                                              select account).AnyAsync();
+                if (isUsernameTaken)
+                {
+                    ModelState.AddModelError(nameof(RegisterViewModel.Username), "Username is already subscribed.");
+                }
+
+                if (isEmailTaken || isUsernameTaken)
+                {
+                    return View(reg);
+                }
+
                 // map data to user account instance
                 UserAccount acc = new UserAccount()
                 {
