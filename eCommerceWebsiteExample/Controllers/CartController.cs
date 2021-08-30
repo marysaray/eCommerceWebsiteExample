c69fb1123511ee0a1fb60f625c778d162af827b5
Mary Saray <msaray.ms@outlook.com>
2021-08-30 16:52:20 -0700

    Implemented single product to cart

---
+++ eCommerceWebsiteExample/Controllers/CartController.cs
@@ -0,0 +1,55 @@
+﻿using eCommerceWebsiteExample.Data;
+using eCommerceWebsiteExample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceWebsiteExample.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ProductContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+
+        public CartController(ProductContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        /// <summary>
+        /// Adds a product to the shopping cart
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> Add(int id) // Id of product to add
+        {
+            Product p = await ProductDb.GetProductAsync(_context, id);
+
+            // Add product to cart cookie
+            string data = JsonConvert.SerializeObject(p);
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddYears(1),
+                Secure = true,
+                IsEssential = true
+            };
+
+            _httpContext.HttpContext.Response.Cookies.Append("CartCookie", data, options);
+            
+            // Redirect back to previous page
+            return RedirectToAction("Index", "Product");
+        }
+
+        public IActionResult Summary()
+        {
+            // Display all products in the shopping cart cookie
+            return View();
+        }
+    }
+}
