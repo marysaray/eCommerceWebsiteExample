7a68506863f214dc9750fada2dc0a8804d0f04ba
Mary Saray <msaray.ms@outlook.com>
2021-08-30 08:48:13 -0700

    Created Sign In page and added error message to credentials

--- eCommerceWebsiteExample/Controllers/UserController.cs
+++ eCommerceWebsiteExample/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerceWebsiteExample.Data;
 using eCommerceWebsiteExample.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,6 +46,49 @@ namespace eCommerceWebsiteExample.Controllers
             }
 
             return View(reg);
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(LoginViewModel model)
+        {
+            // validate model
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            // Query Syntax
+            //UserAccount account =
+            //    await (from u in _context.UserAccounts
+            //           where (u.Username == model.UsernameOrEmail
+            //           || u.Email == model.UsernameOrEmail)
+            //           && u.Password == model.Password
+            //           select u).SingleOrDefaultAsync();
+            // Method Syntax
+
+            UserAccount account = await _context.UserAccounts
+                    .Where(userAcc => (userAcc.Username == model.UsernameOrEmail ||
+                            userAcc.Email == model.UsernameOrEmail) &&
+                            userAcc.Password == model.Password)
+                    .SingleOrDefaultAsync();
+                     
+            if (account == null)
+            {
+                // Credentials did not match
+
+                // Custom error message
+                ModelState.AddModelError(String.Empty, "Credentials were not found.");
+
+                return View(model);
+            }
+
+            // Log user into website
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
