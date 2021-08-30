11801365e51aeaeba30163eac6a2d0fbffe294d4
Mary Saray <msaray.ms@outlook.com>
2021-08-30 10:11:01 -0700

    Added session management for Sign in page

--- eCommerceWebsiteExample/Controllers/UserController.cs
+++ eCommerceWebsiteExample/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using eCommerceWebsiteExample.Data;
 using eCommerceWebsiteExample.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -48,8 +49,15 @@ namespace eCommerceWebsiteExample.Controllers
             return View(reg);
         }
 
+        [HttpGet]
         public IActionResult SignIn()
         {
+            // Check if user already logged in
+            if (HttpContext.Session.GetInt32("UserId").HasValue)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
         }
 
@@ -87,6 +95,7 @@ namespace eCommerceWebsiteExample.Controllers
             }
 
             // Log user into website
+            HttpContext.Session.SetInt32("UserId", account.UserId);
 
             return RedirectToAction("Index", "Home");
         }
