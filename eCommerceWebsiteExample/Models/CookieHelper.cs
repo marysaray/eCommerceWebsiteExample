3b7e67051f315dbc189c7e76866a99be8e4d9b62
Mary Saray <82470838+marysaray@users.noreply.github.com>
2021-08-30 18:22:01 -0700

    Added display cart item count in navigation (#48)

--- eCommerceWebsiteExample/Models/CookieHelper.cs
+++ eCommerceWebsiteExample/Models/CookieHelper.cs
@@ -50,7 +50,8 @@ namespace eCommerceWebsiteExample.Models
 
         public static int GetTotalCartProducts(IHttpContextAccessor http)
         {
-            throw new NotImplementedException();
+            List<Product> cartProducts = GetCartProducts(http);
+            return cartProducts.Count;
         }
     }
 }
